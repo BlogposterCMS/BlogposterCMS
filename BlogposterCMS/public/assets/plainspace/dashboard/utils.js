fcaf8f7fb9dfa45d6ccfb1bc725aafe725769abb
m41130 <45858598+m41130@users.noreply.github.com>
2025-06-22 13:12:47 +0200

    fix partial paths in dashboard utilities

--- BlogposterCMS/public/assets/plainspace/dashboard/utils.js
+++ BlogposterCMS/public/assets/plainspace/dashboard/utils.js
@@ -1,9 +1,11 @@
 // public/assets/plainspace/dashboard/utils.js
 async function fetchPartial(partialName, partialType = 'headers') {
-  const response = await window.fetchWithTimeout(`/assets/partials/${partialType}/${partialName}.html`);
-    if (!response.ok) {
-      throw new Error(`Partial "${partialName}" (${partialType}) not found.`);
-    }
-    return await response.text();
+  const response = await window.fetchWithTimeout(
+    `/assets/plainspace/partials/${partialType}/${partialName}.html`
+  );
+  if (!response.ok) {
+    throw new Error(`Partial "${partialName}" (${partialType}) not found.`);
   }
+  return await response.text();
+}
   

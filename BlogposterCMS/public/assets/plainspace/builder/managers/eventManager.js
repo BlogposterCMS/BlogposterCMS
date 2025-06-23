f4c7e0bc06861a4084b1d0c40972aac180fe615b
m41130 <45858598+m41130@users.noreply.github.com>
2025-06-23 16:17:45 +0200

    fix builder custom events

--- BlogposterCMS/public/assets/plainspace/builder/managers/eventManager.js
+++ BlogposterCMS/public/assets/plainspace/builder/managers/eventManager.js
@@ -11,7 +11,7 @@ export function registerDeselect(gridEl, state, actionBar, hideToolbar) {
     }
     actionBar.style.display = 'none';
     state.activeWidgetEl.classList.remove('selected');
-    state.activeWidgetEl.dispatchEvent(new Event('deselected'));
+    state.activeWidgetEl.dispatchEvent(new Event('deselected', { bubbles: true }));
     state.activeWidgetEl = null;
     hideToolbar();
     gridEl.__grid.clearSelection();
